b5e741e6e10f4bceb37c1ce292e234fa61e0407a
Chovatiya kenil <chovatiyakenil123@gmail.com>
2024-07-04 16:23:18 +0530

    add: iamges and files

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './Portfolio.css'
 import portfolio_img1 from '../../assets/my-portfolio-image.jpg'
 import portfolio_img2 from '../../assets/netflix-landing-page-image.jpg'
+import portfolio_img3 from '../../assets/my-todo-app-image.jpg'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faLink } from '@fortawesome/free-solid-svg-icons'
 
@@ -37,16 +38,16 @@ const Portfolio = () => {
                     <h4>netflix landing page clone</h4>
                 </div>
                 <div className="work-row">
-                    <img src={portfolio_img1} alt="" />
+                    <img src={portfolio_img3} alt="" />
                     <div className="main-row">
                         <div className="row-text">
                             <h5>To-Do app</h5>
                         </div>
                         <div className="row-icon">
-                            <a href="#" target='_blank'><FontAwesomeIcon icon={faLink} /></a>
+                            <a href={portfolio_img3} target='_blank'><FontAwesomeIcon icon={faLink} /></a>
                         </div>
                     </div>
-                    <h4>to-do list application</h4>
+                    <h4>to-do list application using REACT.JS & TAILWINDCSS</h4>
                 </div>
             </div>
             {/* <a href="#" className="btn">See more</a> */}
