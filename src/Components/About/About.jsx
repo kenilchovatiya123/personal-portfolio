6895e76c970d298da40c710e9903af269007f6dd
Chovatiya kenil <chovatiyakenil123@gmail.com>
2024-09-13 14:54:02 +0530

    initial commit

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -14,7 +14,7 @@ const About = () => {
                     <div className="about-col-2">
                         <h1 className="sub-title">About Me</h1>
                         <div className="tab-para">
-                            <p>Hi,  I'm Kenil Chovatiya, as a fresher with strong foundation in HTML, CSS, and JavaScript, I specialize in creating responsive, user-friendly web interfaces that deliver an exceptional user experience. I enjoy transforming ideas into reality through clean, efficient code and modern design principles.</p>
+                            <p>Hi,  I'm Kenil Chovatiya, as a fresher with a strong foundation in HTML, CSS, and JavaScript, I specialize in creating responsive, user-friendly web interfaces that deliver an exceptional user experience. I enjoy transforming ideas into reality through clean, efficient code and modern design principles.</p>
 
                             <p>I am constantly learning and staying updated with the latest trends and technologies in front-end development to deliver cutting-edge solutions. Whether working on a team or independently, I am committed to producing high-quality, maintainable code that meets project goals and exceeds expectations.</p>
                         </div>
@@ -52,7 +52,7 @@ const About = () => {
                             <div className="progress">
                                 <span className="skill"><span>Tailwind</span> <i className="val">75%</i></span>
                                 <div className="progress-bar-wrap">
-                                    <div style={{ width: "60%" }} className="progress-bar" role="progressbar"></div>
+                                    <div style={{ width: "600%" }} className="progress-bar" role="progressbar"></div>
                                 </div>
                             </div>
 
